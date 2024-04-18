62c6379583b3d3906434e03fdc702001eda726d3
2sandeep7 <ssandeep8696@gmail.com>
2024-04-18 14:53:04 -0500

    type error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route,  } from "react-router-dom"
 import Signup from "./pages/Signup"
 import Signin from "./pages/Signin"
 import Blog from "./pages/Blog"
