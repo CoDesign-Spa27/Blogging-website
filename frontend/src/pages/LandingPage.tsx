bf162d2f4ca312bc6538dbc1c0d1707c16c2142b
2sandeep7 <ssandeep8696@gmail.com>
2024-04-20 16:41:19 -0500

    go to blogs button

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -28,7 +28,7 @@ const LandingPage = () => {
         </div>
         <div className="py-4">
         <button 
-        onClick={()=>navigate('/blogs')}
+        onClick={()=>navigate('/signup')}
         className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
 <span className="relative md:px-5 px-2 py-2 md:py-2.5 transition-all  ease-in duration-300 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
 Take Flight To Blogs
